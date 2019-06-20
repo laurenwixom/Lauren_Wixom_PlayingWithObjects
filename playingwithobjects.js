3b9c65a21b59683c0ce9b39ca8b9bb66d14549be
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-19 23:31:05 -0500

    Add files via upload

---
+++ playingwithobjects.js
@@ -0,0 +1,33 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+var age;
+
+if((john[0].age = 30 )){
+  console.log =  age[0];
+}
+ 
+
+
+
+
+
+console.log(users.name[0]); 
+
+
+
+
+
+
+var nameage = 0;
+for (var nameage in users){
+  console.log(users[name].type);
+  if((michael[name].age < 25 && michael[name].name == 'Michael') || michael[name].age == '37'){
+    numeage++;
+  }
+  else {
+    console.log(users[name] "John", users[age] "30");
+    console.log(users[name] "David", users[age] "27") 
+   }
+}
+console.log(nameage);
+
